15bd4fa5edcd4fca118bcdee364bcc241553c101
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2020-05-15 07:52:59 +0200

    i have completed skin diagnosis

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 # coding=utf-8
+
 import sys
 import os
 import glob
@@ -7,58 +8,62 @@ import re
 import numpy as np
 
 # Keras
-# from keras.applications.imagenet_utils import preprocess_input, decode_predictions
-# from keras.models import load_model
-# from keras.preprocessing import image
 
-# Flask utils
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+ 
+# # Flask utils
 from flask import Flask, redirect, url_for, request, render_template, redirect
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
- 
-from flask import request, redirect
-
-
-import os
-
-# Define a flask app
-
-
 
 app = Flask(__name__)
-app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["JPEG", "JPG", "PNG", "GIF"]
-MODEL_PATH = 'models/model.h5'
+
 app.config["IMAGE_UPLOADS"] = "images"
-# Load your trained model
-# model = load_model(MODEL_PATH)
+
  
 print('Model loaded. Check http://127.0.0.1:5000/')
-def allowed_image(filename):
+
+def predictskin(imagepath):
+
+    MODEL_PATH = 'models/model.h5'
+    model = load_model(MODEL_PATH)
+    print("hi")
+    test_image = image.load_img(imagepath, target_size = (75, 100)) 
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = model.predict(test_image)
+    return result
+
+# def predictbone(imagepath):
+#     MODEL_PATH = 'models/scratchModel.h5'
+#     model = load_model(MODEL_PATH)
+#     print("hi")
+#     test_image = image.load_img(imagepath, target_size = (75, 100)) 
+#     test_image = image.img_to_array(test_image)
+#     test_image = np.expand_dims(test_image, axis = 0)
+#     result = model.predict(test_image)
+#     return result
+
+def predictpneumonia(imagepath):
     
-    if not "." in filename:
-        return False
-
-    ext = filename.rsplit(".", 1)[1]
-
-    if ext.upper() in app.config["ALLOWED_IMAGE_EXTENSIONS"]:
-        return True
-    else:
-        return False
-
-# test_image = image.load_img("images/img.jpg", target_size = (75, 100)) 
-# test_image = image.img_to_array(test_image)
-# test_image = np.expand_dims(test_image, axis = 0)
-
-# #predict the result
-# result = model.predict(test_image)
-result=0
-print(result)
+    MODEL_PATH = 'models/model123.h5'
+    model = load_model(MODEL_PATH)
+    print("hi")
+    test_image = image.load_img(imagepath, target_size = (75, 100)) 
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    result = model.predict(test_image)
+    return result
+    
 @app.route('/', methods=['GET'])
 def index():
     # Main page
-    return render_template("index.html",result=result)
+    
+    return render_template("index.html")
 
 
 @app.route("/upload-image", methods=["GET", "POST"])
@@ -69,13 +74,17 @@ def upload_image():
         if request.files:
 
             image = request.files["image"]
-
-            image.save(os.path.join(app.config["IMAGE_UPLOADS"], image.filename))
-
+            filepath=os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+            print(filepath)
+            image.save(filepath)
             print("Image saved")
-
-            return redirect(request.url)
+            print(filepath)
+            result=predictskin(filepath)
+            print(result)
+            return render_template("index.html",result=result) 
 
     return render_template("index.html")  
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,threaded=False)
+
