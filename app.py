5703215ab1ed46e7a58283425f232dc9afa446ae
mernaadell <mernaadel963@gmail.com>
2020-08-11 18:55:29 +0200

    My Final Work Now you can use my web site to detect any disease

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from __future__ import division, print_function
-# coding=utf-8
 
 import sys
 import os
@@ -30,7 +28,7 @@ app.config["IMAGE_UPLOADS"] = "images"
 # test_image = np.expand_dims(test_image, axis = 0)
 # prediction = model.predict(test_image)
 # print(prediction)
- 
+
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 def preparepic(filepath,size1,size2):
@@ -77,17 +75,78 @@ def upload_image():
         if request.files:
 
             image = request.files["image"]
-            filepath=os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+            app.config["filepath"]=os.path.join(app.config["IMAGE_UPLOADS"], image.filename)
+            filepath=app.config["filepath"]
             print(filepath)
             image.save(filepath)
             print("Image saved")
             print(filepath)
-            result=predictbone(filepath)
-            print(result)
-            return render_template("index.html",result=result) 
+            # predictskin(filepath)
+            
+            return render_template("index.html") 
 
     return render_template("index.html")  
+@app.route("/upload-image/skin", methods=["GET", "POST"])
+def skin_cancer():
+    arr=["1. Melanocytic nevi","Melanoma","Benign keratosis-like lesions","Basal cell carcinoma","Actinic keratoses","Vascular lesions"
+,"Dermatofibroma"]
+    app.config["filepath"]
+    print(app.config['filepath'])
+    if(app.config["filepath"]!=""):
+        print("hi")
+        result=predictskin(app.config["filepath"])
+        print(type(result))
+        count=0;
+        for i in result:
+            for x in i:
+                if(x>=1):
+                    print(arr[count])
+                    result=arr[count]
+                    break
+                count=count+1
+        
+        return render_template("index.html",result=result) 
+    # else:print(app.config['filepath'])
+    return render_template("index.html",result=result)  
+@app.route("/upload-image/bone", methods=["GET", "POST"])
+def bone_dig():
+    app.config["filepath"]
+    print(app.config['filepath'])
+    if(app.config["filepath"]!=""):
+        print("hi")
+        result=predictbone(app.config['filepath'])
+        print(result)
+        arr=[ "bones abnormality classification","elbow ","_finger_hand_humerus_forearm_shoulder_wrist"]
+       
+        count=0;
+        for i in result:
+            for x in i:
+
+                if(x>=1):
+                    print(arr[count])
+                    result=arr[count]
+                    break
+                count=count+1
+
+        return render_template("index.html",result=result) 
+    # else:print(app.config['filepath'])
+    return render_template("index.html",result=result)  
+@app.route("/upload-image/pneumonia", methods=["GET", "POST"])
+def pen_dig():
+    print(app.config['filepath'])
+    if(app.config["filepath"]!=""):
+        print("hi")
+        result=predictpneumonia(app.config['filepath'])
+        print(result)
+        arr=[ "normal", "pneumonia"]
+        if (result==0):
+            result=arr[0]
+        else:
+            result=arr[1];
+        return render_template("index.html",result=result) 
+    # else:print(app.config['filepath'])
+    return render_template("index.html",result=result)  
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000,threaded=False)
+    app.run(host="0.0.0.0",port=8000,threaded=False)
 
---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+
+console.log("hi")
+document.querySelector(".loader").style.display="none";
+document.querySelector("#fb").addEventListener('click',function(){
+ console.log("hi merna")
+    document.querySelector(".loader").style.display="block";
+    
+    });
+document.querySelector(".btn btn-secondary").addEventListener('click',function(){
+    
+
+document.querySelector(".loader").style.display="block";
+
+});
+document.querySelector(".btn btn-success").addEventListener('click',function(){
+
+    document.querySelector(".loader").style.display="block";
+    
+    });
